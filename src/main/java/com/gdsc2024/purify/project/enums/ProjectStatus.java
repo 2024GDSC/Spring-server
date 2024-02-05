1482251f808902fd3dbc54f4ff2cb0ffad5720e5
반영환 <lopahn2@gmail.com>
2024-02-05 11:33:12 +0900

    fix: project status update

--- src/main/java/com/gdsc2024/purify/project/enums/ProjectStatus.java
+++ src/main/java/com/gdsc2024/purify/project/enums/ProjectStatus.java
@@ -7,6 +7,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ProjectStatus {
-    OPEN,
-    DELETED;
+    OPEN, // 모두가 열람 가능
+    PRIVATE, // 공유 리스트만 열람 가능
+    PROHIBITION, // 관리자가 블락을 건 상태
+    DELETE_REQUEST, // 삭제 요청
+    UPDATE_REQUEST, // 수정 요청
+    TERMINATION_REQUEST, // 종료 요청
+    TERMINATED; // 종료된 프로젝트
 }
